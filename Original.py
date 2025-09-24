f9db7b616ca6232a0ea5d74131b39079d6b83d76
RNidhish <nidhish.ramane@etudiant.univ-rennes1.fr>
2025-09-24 12:02:45 +0200

    orginal

---
+++ Original.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+# Lire le fichier CSV
+df = pd.read_csv('imdb_top_1000.csv')
+
+
+# Supprimer les colonnes indésirables
+df = df.drop(columns=['Poster_Link', 'Certificate', 'Overview'])
+
+
+# Conversion des donnée “Runtime” en int
+df["Runtime"] = df['Runtime'].str.extract('(\d+)').astype(int)
+
+
+df.to_csv('imdb_top.csv', index=False)
+
+
+print(df.head())
