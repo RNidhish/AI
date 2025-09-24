4db088848ae85a2d8c4e52c5ddd2c0c057dd2c21
BZHKylian <kylian.houedec.56@gmail.com>
2025-09-24 13:53:30 +0200

    conversion string to int

--- Original copy.py
+++ Original copy.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 
 # Lire le fichier CSV
@@ -8,19 +14,18 @@ df = pd.read_csv('imdb_top_1000.csv')
 # Supprimer les colonnes indésirables
 df = df.drop(columns=['Poster_Link', 'Certificate', 'Overview'])
 
-
 # Conversion des donnée “Runtime” en int
 df["Runtime"] = df['Runtime'].str.extract('(\d+)').astype(int)
+df['Gross'] = df['Gross'].str.replace(',', '')
 
 # Garder les donnée quantitative
 df_quant = df.drop(columns=['Series_Title', 'Genre', 'Director', 'Star1', 'Star2', 'Star3', 'Star4'])
 df_quant.to_csv('imdb_top_quant.csv', index=False)
 
-# Garder les donnée quanlitative
+# Garder les donnée qualitative
 df_qual = df.drop(columns=['Released_Year', 'Runtime', 'IMDB_Rating', 'Meta_score', 'No_of_Votes', 'Gross'])
 df_qual.to_csv('imdb_top_qual.csv', index=False)
 
 df.to_csv('imdb_top.csv', index=False)
-
 
 print(df.head())
